f52365ff1abdfc6ebfc8a533aff056b27d1feca9
Shanukaudaya <162829235+Shanukaudaya@users.noreply.github.com>
2024-03-10 21:50:31 +0530

    Update config.js
    Bot

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94767487841', 'me', true],
+  ['94767487841', 'shanuka udaya', true], 
   [''] 
 ] //Number of owners
 
